b04bba0bb842e27e8d9ebfff4b78e145a290339d
eeasee <35984104+X-Green@users.noreply.github.com>
2025-05-02 22:59:14 +0800

    Add RGB colors

--- maglev2025_software/Tasks/WS2812Task.hpp
+++ maglev2025_software/Tasks/WS2812Task.hpp
@@ -10,8 +10,19 @@
 namespace Tasks::WS2812Task
 {
 
+const uint8_t RED[3]    = {255, 0, 0};
+const uint8_t ORANGE[3] = {255, 127, 0};
+const uint8_t YELLOW[3] = {255, 255, 0};
+const uint8_t GREEN[3]  = {0, 255, 0};
+const uint8_t CYAN[3]   = {0, 255, 255};
+const uint8_t BLUE[3]   = {0, 0, 255};
+const uint8_t PURPLE[3] = {127, 0, 255};
+const uint8_t WHITE[3]  = {255, 255, 255};
+const uint8_t BLACK[3]  = {0, 0, 0};
+const uint8_t PINK[3]   = {255, 0, 255};
+
 const int LED_COUNT = 8;
-inline uint8_t colors[LED_COUNT][3]; // R,G,B
+inline uint8_t colors[LED_COUNT][3];  // R,G,B
 
 void initWS2812();
 void updateWS2812();
