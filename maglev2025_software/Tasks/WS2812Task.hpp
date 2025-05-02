d75e8d6bebfac1ff1aabc9e19aa3e64ba2cccc9f
eeasee <35984104+X-Green@users.noreply.github.com>
2025-05-02 23:01:56 +0800

    Add RGB colors

--- maglev2025_software/Tasks/WS2812Task.hpp
+++ maglev2025_software/Tasks/WS2812Task.hpp
@@ -10,16 +10,16 @@
 namespace Tasks::WS2812Task
 {
 
-const uint8_t RED[3]    = {255, 0, 0};
-const uint8_t ORANGE[3] = {255, 127, 0};
-const uint8_t YELLOW[3] = {255, 255, 0};
-const uint8_t GREEN[3]  = {0, 255, 0};
-const uint8_t CYAN[3]   = {0, 255, 255};
-const uint8_t BLUE[3]   = {0, 0, 255};
-const uint8_t PURPLE[3] = {127, 0, 255};
-const uint8_t WHITE[3]  = {255, 255, 255};
-const uint8_t BLACK[3]  = {0, 0, 0};
-const uint8_t PINK[3]   = {255, 0, 255};
+const uint8_t RGB_RED[3]    = {255, 0, 0};
+const uint8_t RGB_ORANGE[3] = {255, 127, 0};
+const uint8_t RGB_YELLOW[3] = {255, 255, 0};
+const uint8_t RGB_GREEN[3]  = {0, 255, 0};
+const uint8_t RGB_CYAN[3]   = {0, 255, 255};
+const uint8_t RGB_BLUE[3]   = {0, 0, 255};
+const uint8_t RGB_PURPLE[3] = {127, 0, 255};
+const uint8_t RGB_WHITE[3]  = {255, 255, 255};
+const uint8_t RGB_BLACK[3]  = {0, 0, 0};
+const uint8_t RGB_PINK[3]   = {255, 0, 255};
 
 const int LED_COUNT = 8;
 inline uint8_t colors[LED_COUNT][3];  // R,G,B
